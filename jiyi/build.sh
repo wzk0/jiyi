c558f5e1d7ab6cb220cbe518a5a4559c33b0ae48
wzk0 <729301277@qq.com>
2025-02-11 23:50:55 +0800

    update build

--- jiyi/build.sh
+++ jiyi/build.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 VERSION=$(sed -n "5s/.*JIYI_VERSION = '\(.*\)'.*/\1/p" main.py)
-export VERSION=$VERSION
+echo $VERSION > JIYI_VERSION
 sh version.sh
 flet build apk --arch="arm64-v8a" --product="记易" --org="org.wzk" --build-version=$VERSION
